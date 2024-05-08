186e6ba362bf1899649effdc071c02fdf8ba0971
asim <asimjaipuri96@gmail.com>
2024-05-08 10:42:22 +0530

    added all screens

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,11 +1,121 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  FlatList,
+  Image,
+  RefreshControl,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { icons, images } from "../../constants";
+import EmptyState from "../components/EmptyState";
+import useAppwrite from "../../lib/useAppWrite";
+import VideoCard from "../components/VideoCard";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { getUserVideos } from "../../lib/video_routes/get_user_videos";
+import { signOut } from "../../lib/user_routes/sign_out";
 
 const Profile = () => {
+  const { user, setuser, setisLoggedIn } = useGlobalContext();
+  const {
+    data: posts,
+    refetch,
+    isLoading,
+  } = useAppwrite(() => getUserVideos(user.$id));
+
+  const [refreshing, setRefreshing] = useState(false);
+  const logout = async () => {
+    await signOut();
+    setuser(null);
+    setisLoggedIn(false);
+
+    router.replace("/sign-in");
+  };
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="font-pbold text-xl">Profile</Text>
-    </View>
+    <SafeAreaView className="bg-primary flex-1">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => {
+          return (
+            <VideoCard
+              title={item.title}
+              thumbnail={item.thumbnail}
+              video={item.video}
+              creator={item.creator.username}
+              avatar={item.creator.avatar}
+            />
+          );
+        }}
+        ListHeaderComponent={() => (
+          <View className="w-full flex justify-center items-center mt-6 mb-12 px-4">
+            <TouchableOpacity
+              onPress={logout}
+              className="flex w-full items-end mb-10"
+            >
+              <Image
+                source={icons.logout}
+                resizeMode="contain"
+                className="w-6 h-6"
+              />
+            </TouchableOpacity>
+
+            <View className="w-16 h-16 border border-secondary rounded-lg flex justify-center items-center">
+              <Image
+                source={{ uri: user?.avatar }}
+                className="w-[90%] h-[90%] rounded-lg"
+                resizeMode="cover"
+              />
+            </View>
+            <View className={"mt-5"}>
+              <Text className={`text-white text-center font-psemibold text-lg`}>
+                {user.username}
+              </Text>
+            </View>
+
+            <View className="mt-5 flex flex-row">
+              <View className={"mr-10"}>
+                <Text
+                  className={`text-white text-center font-psemibold text-xl`}
+                >
+                  {posts.length || 0}
+                </Text>
+                <Text className="text-sm text-gray-100 text-center font-pregular">
+                  Posts
+                </Text>
+              </View>
+              <View className={"ml-10"}>
+                <Text
+                  className={`text-white text-center font-psemibold text-xl`}
+                >
+                  1.2k
+                </Text>
+                <Text className="text-sm text-gray-100 text-center font-pregular">
+                  Followers
+                </Text>
+              </View>
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title={"No videos found"}
+            subtitle={"Be the first one to upload a video"}
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </SafeAreaView>
   );
 };
 
--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,11 +1,81 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  FlatList,
+  Image,
+  RefreshControl,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { router, useLocalSearchParams } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import SearchInput from "../components/SearchInput";
+import EmptyState from "../components/EmptyState";
+import useAppwrite from "../../lib/useAppWrite";
+import VideoCard from "../components/VideoCard";
+import { getSearchVideos } from "../../lib/video_routes/search_videos";
 
 const Search = () => {
+  const { query } = useLocalSearchParams();
+  const {
+    data: posts,
+    refetch,
+    isLoading,
+  } = useAppwrite(() => getSearchVideos(query));
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView className="bg-primary flex-1">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => {
+          return (
+            <VideoCard
+              title={item.title}
+              thumbnail={item.thumbnail}
+              video={item.video}
+              creator={item.creator.username}
+              avatar={item.creator.avatar}
+            />
+          );
+        }}
+        ListHeaderComponent={() => (
+          <View className="flex my-6 px-4 space-y-6">
+            <View className="flex justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Search Results
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  {query}
+                </Text>
+              </View>
+
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+
+            <SearchInput initialQuery={query} />
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title={"No videos found"}
+            subtitle={"No videos found for this query"}
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
